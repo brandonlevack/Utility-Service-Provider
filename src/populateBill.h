052388e232f7ff3d44d1a02d212ac054a350e371
mpontoni <159088397+mpontoni@users.noreply.github.com>
2025-04-01 03:01:40 -0400

    Update populateBill.h

--- src/populateBill.h
+++ src/populateBill.h
@@ -1,13 +1,11 @@
-#ifndef BILLDATALOADER_H
-#define BILLDATALOADER_H
+#ifndef POPULATEBILL_H
+#define POPULATEBILL_H
 
 #include <vector>
 #include <sqlite3.h>
 #include "Bill.h"
 
-class BillDataLoader {
-public:
     static std::vector<Bill> loadAllBills(const char* dbPath);
-};
+
 
 #endif
