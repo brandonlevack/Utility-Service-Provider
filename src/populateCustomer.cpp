563847a6ff2f7c66d056ceb58a46ee7cabc08881
mpontoni <159088397+mpontoni@users.noreply.github.com>
2025-03-31 20:29:03 -0400

    Update populateCustomer.cpp

--- src/populateCustomer.cpp
+++ src/populateCustomer.cpp
@@ -2,6 +2,45 @@
 #include <sqlite3.h>
 #include <iostream>
 
-std::vector<Customer> populateCustomers() {
+static int customerCallback(void* data, int argc, char** argv, char** azColName) {
+    std::vector<Customer>* customers = static_cast<std::vector<Customer>*>(data); //sets parameter *data to vector pointer
+    Customer customer;
 
+    //splits fullname into first and last name, sets each
+    std::string fullName = std::string(argv[1]);
+    size_t spacePos = fullName.find(" ");
+    if (spacePos != std::string::npos) {
+        customer.setFirstName(fullName.substr(0, spacePos));
+        customer.setLastName(fullName.substr(spacePos + 1));
+    }
+
+    //sets address
+    customer.setStreetAddress(argv[2]);
+    customers->push_back(customer);
+    return 0;
+}
+
+std::vector<Customer> populateCustomers(const char* dbPath) {
+    std::vector<Customer> customers;
+    sqlite3* db;
+    char* errMsg = nullptr;
+
+    //open database coonection and check for error opening
+    if (sqlite3_open(dbPath, &db) != SQLITE_OK) {
+        std::cerr << "Cannot open database: " << sqlite3_errmsg(db) << std::endl;
+        return customers;
+    }
+
+    //sql statement
+    const char* sql = "SELECT customer_id, name, address, service FROM customers;";
+    int rc = sqlite3_exec(db, sql, customerCallback, &customers, &errMsg);
+
+    //sql statment execution error checking
+    if (rc != SQLITE_OK) {
+        std::cerr << "SQL error: " << errMsg << std::endl;
+        sqlite3_free(errMsg);
+    }
+
+    sqlite3_close(db);
+    return customers;
 }
