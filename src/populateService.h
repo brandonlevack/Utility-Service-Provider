6002b5a6dba6c3f3d6f52a7fea6fa045d3dde787
mpontoni <159088397+mpontoni@users.noreply.github.com>
2025-04-01 03:09:38 -0400

    Update populateService.h

--- src/populateService.h
+++ src/populateService.h
@@ -1,13 +1,11 @@
-#ifndef SERVICEDATALOADER_H
-#define SERVICEDATALOADER_H
+#ifndef POPULATESERVICE_H
+#define POPULATESERVICE_H
 
 #include <vector>
 #include <sqlite3.h>
 #include "Service.h"
 
-class ServiceDataLoader {
-public:
     static std::vector<Service> loadAllServices(const char* dbPath);
-};
+
 
 #endif
