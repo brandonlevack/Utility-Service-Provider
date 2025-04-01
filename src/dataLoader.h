8d7ebf4ee6bf33609356fbfaf3d8e87a2734ae13
mpontoni <159088397+mpontoni@users.noreply.github.com>
2025-04-01 02:44:35 -0400

    Create dataLoader.h

---
+++ src/dataLoader.h
@@ -0,0 +1,18 @@
+#ifndef DATALOADER_H
+#define DATALOADER_H
+
+#include <vector>
+#include "Customer.h"
+#include "Provider.h"
+#include "Service.h"
+#include "Bill.h"
+#include <sqlite3.h>
+
+class DataLoader {
+public:
+    static void loadServicesForAll(std::vector<Customer>& customers, const char* dbPath);
+    static void loadBillsForAll(std::vector<Customer>& customers, const char* dbPath);
+    static void loadServicesForAllProviders(std::vector<Provider>& providers, const char* dbPath);
+};
+
+#endif
